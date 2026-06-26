e843949a8c7235ec2960dd7504bc8ae5e4733690
Anurag Kumar <142084822+Anuragkumar103@users.noreply.github.com>
2026-06-26 23:51:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ const { GoogleGenerativeAI } = require("@google/generative-ai");
 const app = express();
 const PORT = process.env.PORT || 5000;
 const API_KEY = process.env.GEMINI_API_KEY;
-
 app.use(cors());
 app.use(bodyParser.json());
 
